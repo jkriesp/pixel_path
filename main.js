4dcbf8ceb88b1ddb9dd00f3266f3d7428c5e4141
Jens Kristian Espevik <jenskristian@gmail.com>
2018-10-14 11:47:51 +0200

    check for valid user input

--- main.js
+++ main.js
@@ -2,15 +2,25 @@ const container = document.querySelector('.container');
 const resetButton = document.querySelector('#reset')
 let square = document.createElement('div');
 
-let fixed_size = 400;
-let rowSquares = 3;
+let fixed_size = 640;
+let rowSquares = 8;
 
 makeGrid();
 
 resetButton.addEventListener('click', function () {
-    rowSquares = prompt("Grid size?");
-    removeGrid();
-    makeGrid();
+    try {
+        rowSquares = prompt("How many squares per side?");
+        if (rowSquares == "") throw "empty";
+        if (rowSquares > 64) throw "too high";
+        if (rowSquares < 2) throw "too low";
+        removeGrid();
+        makeGrid();
+    } catch (error) {
+        alert("Please input a value from 2-64. Error: " + error);
+    }
+
+
+
 
 });
 
@@ -21,49 +31,39 @@ function removeGrid() {
 }
 
 function makeGrid() {
-    let size = fixed_size/ rowSquares;
-    container.style.width = fixed_size +"px";
+    let size = fixed_size / rowSquares;
+    container.style.width = fixed_size + "px";
     container.style.height = fixed_size + "px";
 
     for (let i = 0; i < rowSquares; i++) {
         for (let j = 0; j < rowSquares; j++) {
             square = document.createElement('div');
             square.setAttribute('id', 'square');
-            square.setAttribute('row-id', i);
             square.style.cssFloat = "left";
-            square.style.background = "white";
+            square.style.background = "whitesmoke";
             square.style.opacity = "1.0";
             square.style.width = size + "px";
             square.style.height = size + "px";
             container.appendChild(square);
 
         }
-        // square = document.createElement('div');
-        // square.setAttribute('id', 'square');
-        // square.setAttribute('column-id', i);
-        // square.style.background = "white";
-        // square.style.opacity = "1.0";
-        // square.style.width = size + "px";
-        // square.style.height = size + "px";
-        // container.appendChild(square); 
     }
     const cells = document.querySelectorAll('#square');
 
     cells.forEach((cell) => {
         cell.addEventListener('mouseenter', (e) => {
-            if(e.target.style.background !== 'white') {
-                if(e.target.opacity < 0) return;
+            // Fade to black on each mouseenter
+            if (e.target.style.background !== 'whitesmoke') {
+                if (e.target.opacity < 0) return;
                 e.target.style.opacity = e.target.style.opacity - 0.1;
-                
             } else {
                 e.target.style.background = addRandomColor();
             }
-            
         });
     });
 }
 
-function addBlack () {
+function addBlack() {
     console.log(arguments[0]);
 }
 
